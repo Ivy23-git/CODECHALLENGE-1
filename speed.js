f265b496b1092482b0729393b8e4fcd94d5aa1c8
ivymarubu <ivymarubu@gmail.com>
2023-03-22 15:03:59 +0300

    first commit

---
+++ speed.js
@@ -0,0 +1,19 @@
+document.querySelector('form').addEventListener('submit', function(e) {
+    e.preventDefault(); // This is for form sub
+ 
+    // The confirmtion of speed
+    let vehiclespeed = parseInt(document.getElementById('speed').value);
+ 
+    // do away with point execution
+    let Points = 0;
+    if (vehiclespeed <= 70) {
+     document.getElementById('result').textContent = "Ok";
+    } else {
+     Points = Math.floor((vehiclespeed - 70) / 5);
+     if (Points > 12) {
+      document.getElementById('result').textContent = "License suspended";
+     } else {
+      document.getElementById('result').textContent = "Your vehicle speeding points are, " + Points;
+     }
+    }}
+);
